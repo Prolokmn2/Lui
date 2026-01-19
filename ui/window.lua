931e43f9d6c63f385fa4327333eb8de08375ea16
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:47:19 +0100

    Update window.lua

--- ui/window.lua
+++ ui/window.lua
@@ -1,1 +1,14 @@
+local Window = {}
 
+function Window:Create(title)
+    local gui = Instance.new("ScreenGui")
+    local frame = Instance.new("Frame", gui)
+    frame.Size = UDim2.fromOffset(300,200)
+
+    local label = Instance.new("TextLabel", frame)
+    label.Text = title
+
+    return gui, frame
+end
+
+return Window
