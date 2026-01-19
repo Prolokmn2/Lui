3adf72e34bc204c5c9c1894b454798a3cab89d5e
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:43:48 +0100

    Update system.lua

--- ui/keysystem/system.lua
+++ ui/keysystem/system.lua
@@ -1,1 +1,23 @@
+local Config = require(game.ReplicatedStorage.Lui.config.config)
 
+local KeySystem = {}
+
+function KeySystem:Validate(input)
+    if not Config.Keysystem.Enabled then
+        return true
+    end
+
+    local hashed = ""
+    for i,v in ipairs(Config.Keysystem.Key:split("")) do
+        hashed ..= string.char(string.byte(v) * Config.Keysystem.Multiplier)
+    end
+
+    local attempt = ""
+    for i,v in ipairs(input:split("")) do
+        attempt ..= string.char(string.byte(v) * Config.Keysystem.Multiplier)
+    end
+
+    return attempt == hashed
+end
+
+return KeySystem
