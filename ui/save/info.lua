01d11dc6da972a046ff1933822d8f244d2ee8f97
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:45:25 +0100

    Update info.lua

--- ui/save/info.lua
+++ ui/save/info.lua
@@ -1,1 +1,13 @@
+local Players = game:GetService("Players")
 
+local Info = {}
+
+function Info:Get()
+    local plr = Players.LocalPlayer
+    return {
+        name = plr.Name,
+        userid = plr.UserId
+    }
+end
+
+return Info
