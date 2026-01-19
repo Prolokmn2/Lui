4cb3c51082f12eeb9e88bce0c5af167653778e9f
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:44:18 +0100

    Update hide.lua

--- ui/keysystem/hide.lua
+++ ui/keysystem/hide.lua
@@ -1,1 +1,11 @@
+local Hide = {}
 
+function Hide:Obfuscate(str)
+    local new = ""
+    for _,c in ipairs(str:split("")) do
+        new ..= string.char(string.byte(c) + math.random(3,12))
+    end
+    return new
+end
+
+return Hide
