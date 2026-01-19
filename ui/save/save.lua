3d5ddbda5dc898c049c1d020702c411f246c0a1a
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:45:07 +0100

    Update save.lua

--- ui/save/save.lua
+++ ui/save/save.lua
@@ -1,1 +1,17 @@
+local HttpService = game:GetService("HttpService")
+local Config = require(game.ReplicatedStorage.Lui.config.config)
 
+local Save = {}
+
+function Save:Load()
+    if not isfile(Config.Save.File) then
+        return {}
+    end
+    return HttpService:JSONDecode(readfile(Config.Save.File))
+end
+
+function Save:Write(data)
+    writefile(Config.Save.File, HttpService:JSONEncode(data))
+end
+
+return Save
