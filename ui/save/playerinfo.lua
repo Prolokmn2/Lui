c0af77682361e154e24e82316f33f17f7ab28160
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:45:59 +0100

    Update playerinfo.lua

--- ui/save/playerinfo.lua
+++ ui/save/playerinfo.lua
@@ -1,1 +1,13 @@
+local Players = game:GetService("Players")
 
+local PlayerInfo = {}
+
+function PlayerInfo:Get()
+    local plr = Players.LocalPlayer
+    return {
+        name = plr.Name,
+        display = plr.DisplayName
+    }
+end
+
+return PlayerInfo
