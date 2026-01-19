ad0fb83f0f952f18e2e6dc1989d2d2082b7b84b3
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:48:19 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
+local Config = require(script.config.config)
+local Init = require(script.ui.init)
+local Debug = require(script.ui.debug)
 
+Debug:Log("Starting Lui")
+
+local window = Init:Start()
+
+Debug:Log("Loaded UI")
