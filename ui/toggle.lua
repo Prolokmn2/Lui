fa31e8d1366338c70505682f8f886f59aa65a45a
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:47:06 +0100

    Update toggle.lua

--- ui/toggle.lua
+++ ui/toggle.lua
@@ -1,1 +1,18 @@
+local Toggle = {}
 
+function Toggle:Create(default, callback)
+    local state = default
+
+    local btn = Instance.new("TextButton")
+    btn.Text = state and "ON" or "OFF"
+
+    btn.MouseButton1Click:Connect(function()
+        state = not state
+        btn.Text = state and "ON" or "OFF"
+        callback(state)
+    end)
+
+    return btn
+end
+
+return Toggle
