f0619a5e69813478c978c2b2e10f67db0cb3f47e
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:46:53 +0100

    Update slider.lua

--- ui/slider.lua
+++ ui/slider.lua
@@ -1,1 +1,20 @@
+local Slider = {}
 
+function Slider:Create(min, max, callback)
+    local value = min
+
+    local frame = Instance.new("Frame")
+    frame.Size = UDim2.fromOffset(120,20)
+
+    local bar = Instance.new("TextButton", frame)
+    bar.Size = UDim2.fromScale(1,1)
+
+    bar.MouseButton1Down:Connect(function()
+        value = math.random(min, max)
+        callback(value)
+    end)
+
+    return frame
+end
+
+return Slider
