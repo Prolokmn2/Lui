f46ae6446faae59d9ba4f9f6635f552b23936e6b
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 21:06:24 +0100

    Update button.lua

--- ui/button.lua
+++ ui/button.lua
@@ -1,15 +1,17 @@
 local Button = {}
 
-function Button:Create(text, callback)
-    local btn = Instance.new("TextButton")
-    btn.Text = text
-    btn.Size = UDim2.fromOffset(120,30)
-
-    btn.MouseButton1Click:Connect(function()
-        callback()
-    end)
-
-    return btn
+function Button:Create(text, callback, parent)
+    if game then
+        local btn = Instance.new("TextButton")
+        btn.Text = text
+        btn.Size = UDim2.fromOffset(120,30)
+        btn.Parent = parent
+        btn.MouseButton1Click:Connect(callback)
+        return btn
+    else
+        print("[UI BUTTON]", text, "(click triggers callback in console)")
+        return {Click=function() callback() end}
+    end
 end
 
 return Button
