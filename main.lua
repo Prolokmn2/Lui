cda86de4e923e703e9a4ae2ffcb484448dafa4bb
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 21:08:35 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
-local Config = require(script.config.config)
+local Config = {
+    Save = { File = "LuiSave.json", AutoPlayerInfo = true },
+    Keybind = { ToggleUI = "RightShift" },
+    Keysystem = { Enabled = true, Key = "prolokmn", Multiplier = 7 }
+}
 local Init = require(script.ui.init)
 local Debug = require(script.ui.debug)
 
