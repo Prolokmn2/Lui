513347f484ad4faa47ef6be4be3b9e89ad868afd
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:43:27 +0100

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,1 +1,18 @@
+local Config = {}
 
+Config.Save = {
+    File = "LuiSave.json",
+    AutoPlayerInfo = true
+}
+
+Config.Keybind = {
+    ToggleUI = Enum.KeyCode.RightShift
+}
+
+Config.Keysystem = {
+    Enabled = true,
+    Key = "prolokmn",
+    Multiplier = 7
+}
+
+return Config
