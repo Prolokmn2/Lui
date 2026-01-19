91ed6c48b03d29a06b8349d7e2672e71976587d4
Prolokmn2 <proprolokmn@gmail.com>
2026-01-19 20:46:34 +0100

    Update button.lua

--- ui/button.lua
+++ ui/button.lua
@@ -1,1 +1,15 @@
+local Button = {}
 
+function Button:Create(text, callback)
+    local btn = Instance.new("TextButton")
+    btn.Text = text
+    btn.Size = UDim2.fromOffset(120,30)
+
+    btn.MouseButton1Click:Connect(function()
+        callback()
+    end)
+
+    return btn
+end
+
+return Button
